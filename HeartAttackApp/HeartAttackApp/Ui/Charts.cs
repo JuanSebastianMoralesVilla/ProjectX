6582fa812f248e753df1a3f8c015a9e7a3e98a7c
Sebastián Barrera <juan.barrera4@correo.icesi.edu.co>
2021-04-15 19:25:25 -0500

    charge methods are complete

--- HeartAttackApp/HeartAttackApp/Ui/Charts.cs
+++ HeartAttackApp/HeartAttackApp/Ui/Charts.cs
@@ -34,7 +34,51 @@ namespace HeartAttackApp.Ui
             chart4.ChartAreas["ChartArea4"].AxisX.Interval = 1;
             chart5.ChartAreas["ChartArea5"].AxisX.Interval = 1;
 
+            C1(info1);
+            C2(info2);
+            C3(info3);
+            C4(info4);
+            C5(info5);
+        }
 
+        private void C1(List<string[]> info1)
+        {
+            foreach (string[] i in info1)
+            {
+                chart1.Series["s1"].Points.AddXY(i[0], i[1]);
+            }
+        }
+
+        private void C2(List<string[]> info2)
+        {
+            foreach (string[] i in info2)
+            {
+                chart2.Series["s2"].Points.AddXY(i[0], i[1]);
+            }
+        }
+
+        private void C3(List<string[]> info3)
+        {
+            foreach (string[] i in info3)
+            {
+                chart3.Series["s3"].Points.AddXY(i[0], i[1]);
+            }
+        }
+
+        private void C4(List<string[]> info4)
+        {
+            foreach (string[] i in info4)
+            {
+                chart4.Series["s4"].Points.AddXY(i[0], i[1]);
+            }
+        }
+
+        private void C5(List<string[]> info5)
+        {
+            foreach (string[] i in info5)
+            {
+                chart5.Series["s5"].Points.AddXY(i[0], i[1]);
+            }
         }
     }
 }
